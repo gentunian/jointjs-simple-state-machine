cf15c0c352c7ebcceb51218626739716f14ca0b8
sebastian.treu <sebastian.treu@gmail.com>
2020-08-14 00:07:42 -0300

    arrange tests

--- test/window-message-api.test.ts
+++ test/window-message-api.test.ts
@@ -1,16 +1,17 @@
-import { WindowMessageApi, StateMachineMessageCommand, StateMachineMessage } from "../src/api/window-message-api";
+import { WindowMessageApi, WindowMessageApiMessage } from "../src/api/window-message-api";
 import { CellDataNode } from "../src/machine-state-paper";
 import { State, StateMachine } from "../src/state/machine";
+import { MachineApiEventName } from "../src/api/machine-api";
 
 describe("Window Message API test suite.", () => {
     it("should call MachineApi methods when proper message data is received.", (done: any) => {
         const wapi = new WindowMessageApi(window);
         const nodes: CellDataNode[] = [1, 2, 3].map(i => ({ name: `node${i}`, state: State.Created }));
-        const createMessage: StateMachineMessage<CellDataNode> = { command: StateMachineMessageCommand.Create, id: "m0" };
-        const startMessage: StateMachineMessage<CellDataNode> = { command: StateMachineMessageCommand.Start, id: "m0" };
-        const nextMessage: StateMachineMessage<CellDataNode> = { command: StateMachineMessageCommand.Next, id: "m0" };
-        const stopMessage: StateMachineMessage<CellDataNode> = { command: StateMachineMessageCommand.Stop, id: "m0" };
-        const initializeMessage: StateMachineMessage<CellDataNode> = { command: StateMachineMessageCommand.Initialize, id: "m0", data: { nodes } };
+        const createMessage: WindowMessageApiMessage<CellDataNode> = { command: MachineApiEventName.Create, id: "m0" };
+        const startMessage: WindowMessageApiMessage<CellDataNode> = { command: MachineApiEventName.Start, id: "m0" };
+        const nextMessage: WindowMessageApiMessage<CellDataNode> = { command: MachineApiEventName.Next, id: "m0" };
+        const stopMessage: WindowMessageApiMessage<CellDataNode> = { command: MachineApiEventName.Stop, id: "m0" };
+        const initializeMessage: WindowMessageApiMessage<CellDataNode> = { command: MachineApiEventName.Initialize, id: "m0", data: { nodes } };
 
         spyOn(wapi, "create").and.callFake(() => {
             expect(wapi.create).toHaveBeenCalled();
